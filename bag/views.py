20bed24a966480dd215d37bcf09edb4c57580ae8
ElisabethKAndersson <>
2024-01-20 20:12:46 +0000

    Add quantity selector and add button to shop

--- bag/views.py
+++ bag/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 
-
 # Create your views here.
 
-
-def bag(request):
-    """ Shopping bag """
+def view_bag(request):
+    """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
--- senior_fitness/settings.py
+++ senior_fitness/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-elisabethkandersson-elis-68w4lvk5qm.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-elisabethkandersson-elis-7ylu2f4j1a.us2.codeanyapp.com']
 
 
 # Application definition
