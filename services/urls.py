bdef76669fd182109b0404bc6efcc11dedadf902
Elisabeth Andersson <flisanlisan@gmail.com>
2024-01-22 03:29:47 +0000

    Add walks page with text and image and service prices page

--- services/urls.py
+++ services/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('services', views.services, name='services')
+    path('services', views.services, name='services'),
+    path('services_prices', views.services_prices, name='services_prices'),
+    path('walks', views.walks, name='walks'),
 ]
--- services/views.py
+++ services/views.py
@@ -6,3 +6,13 @@ def services(request):
     """ Presenting and selling services """
 
     return render(request, 'services/services.html')
+
+def services_prices(request):
+    """ Presenting and selling services """
+
+    return render(request, 'services/services_prices.html')
+
+def walks(request):
+    """ Presenting and selling services """
+
+    return render(request, 'services/walks.html')
