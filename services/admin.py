614b32fa6b0ef4a27730967bb7e4d3ad9ae880f3
Elisabeth Andersson <flisanlisan@gmail.com>
2024-01-22 04:32:32 +0000

    Add service json, not installed

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Service
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+    )
+
+    ordering = ('name',)
+
+
+admin.site.register(Service, ProductAdmin)
