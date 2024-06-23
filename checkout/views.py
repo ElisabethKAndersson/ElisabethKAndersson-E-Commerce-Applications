fbe132d0542d2ce840307e94c7dca20b825934f4
Elisabeth Andersson <flisanlisan@gmail.com>
2024-06-23 01:46:20 +0000

    Add stripe payment code

--- checkout/views.py
+++ checkout/views.py
@@ -7,12 +7,16 @@ from bag.contexts import bag_contents
 
 import stripe
 
+
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
-    
+
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
@@ -23,10 +27,16 @@ def checkout(request):
     )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51PUZa5Dib5yNC9z0OVdnu1XqPstH7Dzc4Qg8Imv17WH1SjcGrQiyIlZxxU3QEw3UOYgwNOvnsD0mwhckeo0MxqdA00aD5TS5RG',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
